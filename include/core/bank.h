97eb0182fb9a03265b4e219e30256622162708c9
SomeonesDads <nael.i.manurung@gmail.com>
2026-04-21 16:50:20 +0700

    feat: Add centralized money management

---
+++ include/core/bank.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "core/Player.h"
+using n
+namespace Nimonspoli {
+
+// Dari sini semua transaksi biar gampang dilog, itu aja sebenarnya
+class Bank {
+public:
+    void pay(Player& player, int amount) {player += amount;}
+    void collect(Player& player, int amount) {player -= amount;}
+    void transfer(Player& from, Player& to, int amount) {
+        from -= amount;
+        to   += amount;
+    }
+};
+
+} 
